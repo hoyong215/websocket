243e6dff69579bd68530e2b649f6de5829f08491
hoyong215 <hoyong215@naver.com>
2019-10-21 11:47:27 +0900

    Update node.js

--- node.js
+++ node.js
@@ -1,3 +1,57 @@
+var express = require('express');
+var http = require('http');
+var https = require('https');
+var fs = require('fs');
+var privateKey = fs.readFileSync('server.key').toString();
+var certificate = fs.readFileSync('server.crt').toString();
+
+var options = {
+  key : privateKey
+, cert : certificate
+}
+
+var app = express();
+
+// Start server.
+var port = process.env.PORT || 3000; // Used by Heroku and http on localhost
+process.env['PORT'] = process.env.PORT || 4000; // Used by https on localhost
+
+http.createServer(app).listen(port, function () {
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
+
+// Run separate https server if on localhost
+if (process.env.NODE_ENV != 'production') {
+    https.createServer(options, app).listen(process.env.PORT, function () {
+        console.log("Express server listening with https on port %d in %s mode", this.address().port, app.settings.env);
+    });
+};
+
+if (process.env.NODE_ENV == 'production') {
+    app.use(function (req, res, next) {
+        res.setHeader('Strict-Transport-Security', 'max-age=8640000; includeSubDomains');
+        if (req.headers['x-forwarded-proto'] && req.headers['x-forwarded-proto'] === "http") {
+            return res.redirect(301, 'https://' + req.host + req.url);
+        } else {
+            return next();
+            }
+    });
+} else {
+    app.use(function (req, res, next) {
+        res.setHeader('Strict-Transport-Security', 'max-age=8640000; includeSubDomains');
+        if (!req.secure) {
+            return res.redirect(301, 'https://' + req.host  + ":" + process.env.PORT + req.url);
+        } else {
+            return next();
+            }
+    });
+
+};
+
+
+
+
+/*
 var http = require('http');
 var fs = require('fs');
 var express = require('express');
@@ -18,13 +72,7 @@ var port = process.env.PORT || 3000;
 router.get("/", function (req, res) {
 	res.send("<h1>hello heroku node.js world</h1>" + https_options );
 });
-
-http.createServer(app).listen(port, function (req, res) {
-	console.log(new Date() + ' : nodejs page : test');
-	res.writeHead(404);
-	res.end();
-});
-
+*/
 
 
 /*
