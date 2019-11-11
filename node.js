66c80a46836f048ea4ef77391635cfe5cde3e27b
hoyong215 <hoyong215@naver.com>
2019-11-11 13:58:31 +0900

    Update node.js

--- node.js
+++ node.js
@@ -64,11 +64,8 @@ wss.on('connection', function(ws, req) {
 
 				// 웹소켓을 사용하여 브라우저에 응답값 전송
 				console.log(new Date() + ' : N <- X / ' + phoneNum + ' / ' + data);
-				ws.send(data);
+				ws.send(pushMap);
 				console.log(new Date() + ' : U <- N / ' + phoneNum + ' / ' + data);
-				ws.on('message', function incoming(message) {
-					console.log(message);
-				})
 
 			});
 			ws.xClient.on('close', function() {
