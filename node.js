2e9d6e0e6781cbcfe682f8f701d9a77c381545be
hoyong215 <hoyong215@naver.com>
2019-11-11 14:03:50 +0900

    Update node.js

--- node.js
+++ node.js
@@ -52,40 +52,40 @@ wss.on('connection', function(ws, req) {
 		this.setTimeout(600);
 		this.setEncoding('utf8');
 		
-			ws.xClient.on('data', function(data) {
-				var cmd = data.split('|')[0];
-				console.log(new Date() + ' : X -> N : Command : ' + cmd );
+		ws.xClient.on('data', function(data) {
+			var cmd = data.split('|')[0];
+			console.log(new Date() + ' : X -> N : Command : ' + cmd );
 
-				var pushMap=[];
-				for(var i in data.split('|')) {
-					pushMap.push( data.split('|')[i] );
-				}
-				console.log(pushMap);
+			var pushMap = new Array();
+			for(var i in data.split('|')) {
+				pushMap.push( data.split('|')[i] );
+			}
+			console.log(pushMap);
 
-				// 웹소켓을 사용하여 브라우저에 응답값 전송
-				console.log(new Date() + ' : N <- X / ' + phoneNum + ' / ' + data);
-				ws.send(pushMap);
-				console.log(new Date() + ' : U <- N / ' + phoneNum + ' / ' + data);
+			// 웹소켓을 사용하여 브라우저에 응답값 전송
+			console.log(new Date() + ' : N <- X / ' + phoneNum + ' / ' + data);
+			ws.send(pushMap);
+			console.log(new Date() + ' : U <- N / ' + phoneNum + ' / ' + data);
 
-			});
-			ws.xClient.on('close', function() {
-				ws.xClient.end();
-				console.log(new Date() + ' : XCTI Client Closed!!');
-			});
+		});
+		ws.xClient.on('close', function() {
+			ws.xClient.end();
+			console.log(new Date() + ' : XCTI Client Closed!!');
+		});
 
 
-			ws.on('message', function incoming(message) {
+		ws.on('message', function incoming(message) {
 
-				if(message.split('_')[0] == 'CLIENT') {
-					var pushMap = '';
-					pushMap = message.split('_')[2];
-					phoneNum = pushMap;
-				}
+			if(message.split('_')[0] == 'CLIENT') {
+				var pushMap = '';
+				pushMap = message.split('_')[2];
+				phoneNum = pushMap;
+			}
 
-				console.log(new Date() + ' : U -> N / ' + phoneNum + ' / ' + message);
-				ws.xClient.write(message);
-				console.log(new Date() + ' : N -> X / ' + phoneNum + ' / ' + message);
-			});
+			console.log(new Date() + ' : U -> N / ' + phoneNum + ' / ' + message);
+			ws.xClient.write(message);
+			console.log(new Date() + ' : N -> X / ' + phoneNum + ' / ' + message);
+		});
 		
 	});
 
