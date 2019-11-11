c07d9425c10e801668975799d33001140aaf6a5d
hoyong215 <hoyong215@naver.com>
2019-11-11 12:56:49 +0900

    Update node.js

--- node.js
+++ node.js
@@ -58,8 +58,8 @@ wss.on('connection', function(ws, req) {
 			
 			var pushMap = '';
 			for(var i in data.split('|')) {
-				console.log(new Date() + ' : 1 : ' + data[i]);
-				pushMap += data[i]
+				console.log(new Date() + ' : 1 : ' + data.split('|')[i]);
+				pushMap += data.split('|')[i]
 			}
 			console.log(pushMap);
 			phoneNum = pushMap;
